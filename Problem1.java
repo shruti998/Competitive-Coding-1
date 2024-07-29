934f98ef649255a2c91636cbcde34af6a6a49ac3
Shruti Srivastava <srivastava.shru@northeastern.edu>
2024-07-29 14:19:51 -0400

    Done CC-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,34 @@
+// TC O(logn)
+//SC O(1)
 
+class Solution {
+    private int search(int arr[], int n)
+    {
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        while(high-low>=2)
+        {
+             mid=low+(high-low)/2;
+            int midDiff=arr[mid]-mid;
+            int lowdif=arr[low]-low;
+            int hifgdif=arr[high]-high;
+            if(midDiff!=lowdif)
+            {
+                high=mid;
+            }
+            else if( midDiff!=hifgdif)
+            {
+                low=mid;
+            }
+        }
+        return (arr[low]+arr[high])/2;
+    }
+
+
+    int missingNumber(int n, int arr[]) {
+
+        // Your Code Here
+        return search(arr,n);
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,25 @@
+// TC O(nlogn) for adding , O(n) printing element ,O(nlogn) removing element
+//SC O(n)
+import java.util.PriorityQueue;
 
+public class MinHeap {
+    public static void main(String[] args) {
+        // PriorityQueue 
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        // Add elements 
+        minHeap.add(10);
+        minHeap.add(5);
+        minHeap.add(20);
+        minHeap.add(3);
+
+        // Print elements 
+        System.out.println("Min-Heap elements: " + minHeap);
+
+        // Remove elements
+        while (!minHeap.isEmpty()) {
+            // Polling removes the root element (the smallest element)
+            System.out.println("Removed element: " + minHeap.poll());
+        }
+    }
+}
